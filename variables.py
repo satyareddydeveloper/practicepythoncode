ec92afd9713b90fc400115e7007435b5855fcc79
satya <satyareddydeveloper@gmail.com>
2020-11-30 14:41:45 +0000

    variables and datatypes

--- variables.py
+++ variables.py
@@ -1,2 +1,18 @@
-no = 10
+print("hello world")
+no = 9
+No = 10
+no:int = 8
 print(no)
+print(No)
+MyFloat = 20.5
+MyComplex = 1j
+myexponentialno = 10e10
+MyexponentialEno = 10E10
+MyString = "satya"
+mystring = 'satya'
+MyInt = 10
+MyFloatConvertint = float(MyInt)
+print(MyFloatConvertint)
+type(MyFloatConvertint)
+print("kj")
+
